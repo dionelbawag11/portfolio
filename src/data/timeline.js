a661cf6be6f03558358c27076705ef39aa8a9d78
Dionel Bawag <bawagdionel@gmail.com>
2024-04-01 15:53:56 +0800

    update some content in timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,37 +1,30 @@
 export default [
     {
-       year: 'Today',
-       title: 'YouTube Content Creator',
-       duration: '6 years',
-       details: 
-          'My humble attempt at building my version of the digital world one step at a time by documenting my coding journey and creating software engineering and computer science content to help those coming behind me.',
-    },
-    {
          year: 'Today',
-         title: 'Solo Indie Developer',
+         title: 'Self Taught Developer',
+         duration: '6 months',
+         details:
+             "Right after I graduated I immediately study other technologies such as react js and other web technologies and also I certify myself to other programming languages and create personal projects.",
+     },
+     {
+         year: '2023',
+         title: 'New Era University | BSIT | College',
          duration: '4 years',
          details:
-             "I'm a solo indie developer building software for myself and clients to showcase on YouTube. All coding projects are built from the ground up, from planning and designing all the way to solving real-life problems with code.",
+             "I pursued the knowledge that a information technology degree had to offer. I learned about programming, object oriented programming, and aspects of artificial intelligence. ",
      },
      {
          year: '2019',
-         title: 'Software Engineer',
+         title: "AMA Computer College | STEM Student | SHS",
          duration: '2 years',
          details:
-             'I helped build an enterprise application for a Fortune 500 company in an Agile team setting. Our tech stack consisted of TypeSciprt, Angular, Java, Gradle, and a handful of other languages and tools.',
+             'I graduated as a STEM Student instead of ICT Student that more relevant for being a programmer, but I think I am still in the right path even though I am not a ICT Student in Senior High School',
      },
      {
-         year: '2018',
-         title: "Bachelor's Degree in Computer Science",
-         duration: '5 years',
-         details:
-             'I pursued the knowledge that a computer science degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.',
-     },
-     {
-         year: '2013',
+         year: '2015',
          title: 'First Line of Code',
          duration: 'the beginning',
          details:
-             "I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined, especially as someone who never saw themselves as anything other than a blue collar worker.",
+             "I wrote my first line of code when I was a high school student and there was simple html, css, javascript project in our ICT class and that time I realized that being a web developer is kinda cool.",
      },
  ]
